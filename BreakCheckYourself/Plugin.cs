e1efdb4e58ceb93f2f5ef93b28fd61f220ddd310
Jordon Bedwell <jordon@envygeeks.io>
2024-11-15 22:36:34 -0600

    Add a new Plugin idea BreakCheckYourself

---
+++ BreakCheckYourself/Plugin.cs
@@ -0,0 +1,26 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+
+using HarmonyLib;
+using System.Reflection;
+using JetBrains.Annotations;
+using UnityEngine;
+using BepInEx;
+
+namespace ContainerDumper
+{
+    [
+        BepInPlugin(
+            "EnvyGeeks.BreakCheckYourself", "BreakCheckYourself", "0.1.0"
+        )
+    ]
+    public class Plugin: BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(
+                Assembly.GetExecutingAssembly()
+            );
+        }
+    }
+}
