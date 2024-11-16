4fb402af5fc165cae63de94da26aaf516224815d
Jordon Bedwell <jordon@envygeeks.io>
2024-11-15 22:37:16 -0600

    Add AutoMapPins base project

---
+++ AutoMapPins/Plugin.cs
@@ -0,0 +1,15 @@
+﻿// ReSharper disable CheckNamespace
+
+using BepInEx;
+namespace AutoMapPins
+{
+    [
+        BepInPlugin(
+            "EnvyGeeks.AutoMapPins", "AutoMapPins", "0.1.0"
+        )
+    ]
+    public class Plugin : BaseUnityPlugin
+    {
+        // WIP
+    }
+}
